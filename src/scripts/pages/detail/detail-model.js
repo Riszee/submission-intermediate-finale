475c4d98eb4be007d592b3c24040a3db07af1eb3
aufarisz <96284154+Riszee@users.noreply.github.com>
2025-06-18 14:46:41 +0700

    Delete src directory

--- src/scripts/pages/detail/detail-model.js
+++
@@ -1,7 +0,0 @@
-import { getStoryDetail } from '../../data/api';
-
-export default class DetailModel {
-  async getStoryDetail(id, token) {
-    return await getStoryDetail(id, token);
-  }
-}
--- src/scripts/pages/register/register-model.js
+++
@@ -1,7 +0,0 @@
-import { register } from '../../data/api';
-
-export default class RegisterModel {
-  async register({ name, email, password }) {
-    return await register({ name, email, password });
-  }
-}
